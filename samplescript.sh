4491366e6b2675b51aaeca1e911055b85e415b85
RAHULDEVOPS9 <rahul.bollu9@gmail.com>
2022-08-19 13:30:48 +0530

    #1 | test | Rahul

--- samplescript.sh
+++ samplescript.sh
@@ -1,3 +1,21 @@
-read -p 'enter your name: ' name
+#!/bin/bash
 
-echo your name is $name
+if [ $# -lt 2 ]; then
+  echo Input Missing
+  exit 1
+fi
+
+
+echo First Argument = $1
+echo Second Argument = $2
+echo All Arguments = $*
+echo Number of Arguments = $#
+
+SAMPLE() {
+  echo First Argument in Function = $1
+  echo Second  Argument in Function = $2
+  echo All Arguments in Function = $*
+  echo Number of Arguments = $#
+}
+
+SAMPLE 100 200
