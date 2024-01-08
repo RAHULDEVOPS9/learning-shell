79d6e74788a433ffc5e44286ccf274b97411d83f
Rahul <rahul.bollu9@gmail.com>
2024-01-08 17:52:32 +0530

    Added data

--- samplescript.sh
+++ samplescript.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/bash
 
-case $1 in
-apple)
-  echo 'Price = 10'
-  ;;
-banana)
-   echo 'Price =20'
-   ;;
- *)
-   echo 'Item not found'
-   ;;
- esac
+#case $1 in
+#apple)
+#  echo 'Price = 10'
+ # ;;
+#banana)
+ #  echo 'Price =20'
+#   ;;
+# *)
+#   echo 'Item not found'
+#   ;;
+ #esac
+
+
+ if [ -z "$1" ]; then
+   echo Input is empty
+   exit 1
+ fi
+
+ input=$1
+ if [ "$input" = devops ]; then
+   echo Welcome to DevOps Training
+ fi
